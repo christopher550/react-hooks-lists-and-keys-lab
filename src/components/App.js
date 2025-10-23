ca50015e534364e7bbb30d7a99e3b018111f84f0
christopher550 <blstchris25@gmail.com>
2025-10-23 16:18:12 +0300

    Adds src/data/user.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,15 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+import user, { projects } from "../data/user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
+      <Home name={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ProjectList projects={projects} />
     </div>
   );
 }
